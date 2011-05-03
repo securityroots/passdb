2770ec41313375b247b0f47f60edf5eee0e76c7b
Daniel Martin <etd@nomejortu.com>
2011-05-04 00:38:27 +0100

    Implement the first specs

--- spec/passdb_spec.rb
+++ spec/passdb_spec.rb
@@ -1,6 +1,11 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Passdb: vendor search" do
-  it "should fail if the vendor is empty"
-  it "should fail if criteria is empty"
+  it "should fail if the vendor is empty" do
+    lambda{ Passdb::search(:vendor => nil) }.should raise_error(ArgumentError)
+  end
+
+  it "should fail if criteria is empty" do
+    lambda{ Passdb::search(:criteria => nil) }.should raise_error(ArgumentError)
+  end
 end
