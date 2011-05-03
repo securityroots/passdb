6149521b9c18cbca721d42f91c42154356d93064
Daniel Martin <etd@nomejortu.com>
2011-05-04 00:21:06 +0100

    Add a couple of simple specs

--- spec/passdb_spec.rb
+++ spec/passdb_spec.rb
@@ -1,7 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Passdb" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
-  end
+describe "Passdb: vendor search" do
+  it "should fail if the vendor is empty"
+  it "should fail if criteria is empty"
 end
