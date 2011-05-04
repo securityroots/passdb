1af68634118b1009fe213e1226337a70c1df083a
Daniel Martin <etd@nomejortu.com>
2011-05-04 11:10:47 +0100

    We start by implementing two small tasks to query the passdb database

--- lib/passdb/cli.rb
+++ lib/passdb/cli.rb
@@ -4,5 +4,17 @@ require 'thor/actions'
 module Passdb
   class CLI < Thor
     include Thor::Actions
+    def initialize(*)
+      super
+      Thor::Shell::Basic.new
+    end
+
+    desc "vendor", "Search cirt.net's database for default passwords in a given vendor's products"
+    def vendor
+    end
+
+    desc "criteria", "Free-form search of cirt.net's credential database"
+    def criteria
+    end
   end
 end
--- lib/passdb/runner.rb
+++
@@ -1,7 +0,0 @@
-require 'passdb'
-
-class Passdb::Runner
-  def self.run!(*argv)
-    puts "Welcome to Passdb"
-  end
-end
