88340d96dadd2afa54ee8f28b722abd356578839
Daniel Martin <etd@nomejortu.com>
2011-05-04 11:22:22 +0100

    Merge the two tasks into a single @search@ method with two options to search by vendor or free-form criteria

--- lib/passdb/cli.rb
+++ lib/passdb/cli.rb
@@ -9,12 +9,24 @@ module Passdb
       Thor::Shell::Basic.new
     end
 
-    desc "vendor", "Search cirt.net's database for default passwords in a given vendor's products"
-    def vendor
-    end
+    method_option "vendor", :type => :string, :banner => 
+      "Name of the vendor as especified in http://cirt.net/passwords"
+    method_option "criteria", :type => :string, :banner => 
+      "Free-form criteria to submit to cirt.net's password database"
+    desc "search", "Search cirt.net's database for default passwords by vendor or in free from"
+    def search
+      opts = options.dup
+      if opts["vendor"] && opts["criteria"]
+        puts "You can't specify both a vendor and a free-form criteria"
+        exit 1
+      end
 
-    desc "criteria", "Free-form search of cirt.net's credential database"
-    def criteria
+      if !(opts["vendor"] || opts["criteria"])
+        puts "You need to specify either --vendor or --criteria"
+        exit 1
+      end
+
+
     end
   end
 end
