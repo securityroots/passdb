ec13168c71a9d737ac7832ee90a0f023eab6335c
Daniel Martin <etd@nomejortu.com>
2011-05-04 12:11:20 +0100

    Collect the results from the Passdb::search() call and pretty print them

--- lib/passdb/cli.rb
+++ lib/passdb/cli.rb
@@ -28,10 +28,19 @@ module Passdb
         exit 1
       end
       
+      results = nil
       if opts["vendor"]
-        Passdb.search(:vendor => opts["vendor"])
+        results = Passdb.search(:vendor => opts["vendor"])
       else
-        Passdb.search(:criteria => opts["criteria"])
+        results = Passdb.search(:criteria => opts["criteria"])
+      end
+
+      puts "#{results.size} entries were found:"
+      results.each do |entry|
+        puts "  #{entry.name}"
+        entry.attributes.each do |name, value|
+          puts "    #{name} => #{value}"
+        end
       end
     end
   end
